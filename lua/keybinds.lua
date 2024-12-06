c4750898805d9baabccafe1b6cf3b3d3c7690fb2
jdavisvmediao2 <jack.davis@virginmediao2.co.uk>
2024-12-06 13:12:49 +0000

    keybinds.lua update
    adds the which key window

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -1,7 +1,10 @@
-vim.keymap.set("n", "<leader>o", function () --auto focus between windows
+lvim.builtin.which_key.mappings["o"] = {
+  function () --auto focus between windows
   if vim.fn.bufname():match 'NvimTree_' then
     vim.cmd.wincmd 'p'
   else
     vim.cmd( ":NvimTreeFocus" )
   end
-end, { desc = "explorer focus"})
+end,
+  "explorer focus"
+}
